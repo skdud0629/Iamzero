ec1b77e312ec60059995d2519d8e5c7ac6edfa3e
nazero <skdud4081@gmail.com>
2024-02-20 14:55:32 +0900

    add gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation("androidx.room:room-ktx:2.6.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.2.0")
     implementation ("androidx.appcompat:appcompat:1.1.0")
+    implementation ("androidx.fragment:fragment-ktx:1.3.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
