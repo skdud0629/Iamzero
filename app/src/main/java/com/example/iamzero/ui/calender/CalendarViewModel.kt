d1484931b3649a15605be12ccc7b229cc85da9df
nazero <skdud4081@gmail.com>
2024-02-23 00:34:56 +0900

    modify calenderViewModel

--- app/src/main/java/com/example/iamzero/ui/calender/CalendarViewModel.kt
+++ app/src/main/java/com/example/iamzero/ui/calender/CalendarViewModel.kt
@@ -1,17 +1,27 @@
 package com.example.iamzero.ui.calender
 
+import android.app.Application
+import android.content.Context
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.iamzero.data.Memo
 import com.example.iamzero.data.MemoRepository
+import com.example.iamzero.data.MemoRepositoryImpl
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.util.Date
 
-class CalendarViewModel(private val memoRepository: MemoRepository) : ViewModel() {
+class CalendarViewModel(private val memoRepository: MemoRepositoryImpl) : ViewModel() {
 
-    fun getMemo(id: Long) {
+    private val _memoContent: MutableLiveData<String> = MutableLiveData()
+    val memoContent: MutableLiveData<String> get() = _memoContent
+    val date = ""
+    fun getMemo(date: Date) {
         viewModelScope.launch(Dispatchers.IO) {
-            memoRepository.getMemo(id)
+            val memoData= memoRepository.getMemo(date).toString()//비동기적으로 데이터를 가져온 후, 값을 할당해야 함.
+            _memoContent.postValue(memoData)
         }
     }
 
