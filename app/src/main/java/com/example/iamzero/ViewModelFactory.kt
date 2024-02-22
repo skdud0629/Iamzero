fa653bf3a74b02659e205674f5c3b5abd84c03f7
nazero <skdud4081@gmail.com>
2024-02-22 18:17:05 +0900

    add ViewModelFactory

---
+++ app/src/main/java/com/example/iamzero/ViewModelFactory.kt
@@ -0,0 +1,17 @@
+package com.example.iamzero
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.iamzero.data.MemoRepositoryImpl
+import com.example.iamzero.ui.calender.CalendarViewModel
+
+
+class ViewModelFactory(private val repository: MemoRepositoryImpl) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(CalendarViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return CalendarViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
