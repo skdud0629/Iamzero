2b0edb3602d12ffd8f7a8a9daa9ea1e65201b553
nazero <skdud4081@gmail.com>
2024-02-24 02:17:07 +0900

    modify getMemo

--- app/src/main/java/com/example/iamzero/ui/calender/CalendarViewModel.kt
+++ app/src/main/java/com/example/iamzero/ui/calender/CalendarViewModel.kt
@@ -16,18 +16,19 @@ import java.util.Date
 
 class CalendarViewModel(private val memoRepository: MemoRepositoryImpl) : ViewModel() {
 
-    private val _memoContent: MutableLiveData<String> = MutableLiveData()
-    val memoContent: MutableLiveData<String> get() = _memoContent
+    private val _memoContent: MutableLiveData<String?> = MutableLiveData()
+    val memoContent: MutableLiveData<String?> get() = _memoContent
     val date = ""
     fun getMemo(id: Long) {
         viewModelScope.launch(Dispatchers.IO) {
             try {
-                val memoData = memoRepository.getMemo(id).toString() // 비동기적으로 데이터를 가져온 후, 값을 할당해야 함.
+                val memoData = memoRepository.getMemo(id).content// 비동기적으로 데이터를 가져온 후, 값을 할당해야 함.
                 _memoContent.postValue(memoData)
+
             } catch (e: Exception) {
                 // 데이터를 가져오는 동안 예외가 발생한 경우 처리
                 Log.e("Error CalendarViewModel", "Error fetching memo", e)
-               // _memoContent.postValue("Error fetching memo: ${e.message}")
+                _memoContent.postValue(null)
             }
         }
     }
