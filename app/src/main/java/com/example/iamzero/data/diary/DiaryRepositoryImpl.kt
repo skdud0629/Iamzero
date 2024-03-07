98b649ffedbc9d289930ab2cf14dd835518e4f6c
nazero <skdud4081@gmail.com>
2024-03-07 23:05:47 +0900

    add diaryRepositoryImpl

---
+++ app/src/main/java/com/example/iamzero/data/diary/DiaryRepositoryImpl.kt
@@ -0,0 +1,28 @@
+package com.example.iamzero.data.diary
+
+import android.content.Context
+
+
+class DiaryRepositoryImpl(context: Context) : DiaryRepository {
+    private val database: DiaryDatabase
+
+    init {
+        database = DiaryDatabase.getInstance(context)!!
+    }
+
+    override suspend fun getPost(id: Long): Post {
+        return database.postDao().getPost(id)
+    }
+
+    override suspend fun insertPost(post: Post) {
+        return database.postDao().insertPost(post)
+    }
+
+    override suspend fun editPost(post: Post) {
+        return database.postDao().editPost(post)
+    }
+
+    override suspend fun deletePost(post: Post) {
+        return database.postDao().deletePost(post)
+    }
+}
