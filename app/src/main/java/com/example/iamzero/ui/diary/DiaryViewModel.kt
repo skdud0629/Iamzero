076866ed0d30e1978b7d40cb9d9fb5336e696784
nazero <skdud4081@gmail.com>
2024-03-08 01:48:05 +0900

    add DiaryViewModel

--- app/src/main/java/com/example/iamzero/ui/diary/DiaryViewModel.kt
+++ app/src/main/java/com/example/iamzero/ui/diary/DiaryViewModel.kt
@@ -1,4 +1,41 @@
 package com.example.iamzero.ui.diary
 
-class DiaryViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.iamzero.data.diary.DiaryRepositoryImpl
+import com.example.iamzero.data.diary.Post
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class DiaryViewModel( private val diaryRepository: DiaryRepositoryImpl) : ViewModel(){
+
+    private var _diaryContent :MutableLiveData<Post> = MutableLiveData()
+    val diaryContent: MutableLiveData<Post> get() = _diaryContent
+    fun getDiary(id: Long){
+        viewModelScope.launch(Dispatchers.IO){
+            diaryRepository.getPost(id).let{
+                _diaryContent.postValue(it)
+            }
+        }
+    }
+
+    fun writeDiary(post: Post){
+        viewModelScope.launch(Dispatchers.IO){
+            diaryRepository.insertPost(post)
+        }
+    }
+
+    fun editDiary(post: Post){
+        viewModelScope.launch(Dispatchers.IO){
+            diaryRepository.editPost(post)
+        }
+    }
+
+fun deleteDiary(post: Post){
+        viewModelScope.launch(Dispatchers.IO){
+            diaryRepository.deletePost(post)
+        }
+    }
+
 }
