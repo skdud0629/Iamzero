3ac4010d08b377e2100d5e9a73733fb723ac1557
nazero <skdud4081@gmail.com>
2024-03-07 22:25:20 +0900

    add annotation

--- app/src/main/java/com/example/iamzero/data/diary/Post.kt
+++ app/src/main/java/com/example/iamzero/data/diary/Post.kt
@@ -1,6 +1,11 @@
 package com.example.iamzero.data.diary
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
 data class Post(
+    @PrimaryKey
     val id: String,
     val img: String,
     val content: String
