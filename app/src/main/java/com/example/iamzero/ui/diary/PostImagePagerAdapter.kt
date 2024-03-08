7959ef3df5450542c18c397e37129bfacf2d3a86
nazero <skdud4081@gmail.com>
2024-03-08 22:40:36 +0900

    modify PostImagePagerAdapter

--- app/src/main/java/com/example/iamzero/ui/diary/PostImagePagerAdapter.kt
+++ app/src/main/java/com/example/iamzero/ui/diary/PostImagePagerAdapter.kt
@@ -1,13 +1,14 @@
 package com.example.iamzero.ui.diary
 
+import android.content.Context
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.iamzero.R
-import com.example.iamzero.ui.calender.CalendarFragment
-
-class PostImagePagerAdapter(var postContents: ArrayList<Int>) :
+class PostImagePagerAdapter(var postContents: List<Uri>, val context: Context) :
     RecyclerView.Adapter<PostImagePagerAdapter.PagerViewHolder>() {
     inner class PagerViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder
         (LayoutInflater.from(parent.context).inflate(R.layout.item_post_content, parent, false)) {
@@ -19,6 +20,10 @@ class PostImagePagerAdapter(var postContents: ArrayList<Int>) :
     override fun getItemCount(): Int = postContents.size
 
     override fun onBindViewHolder(holder: PagerViewHolder, position: Int) {
-        holder.postContent.setImageResource(postContents[position])
+        //holder.postContent.setImageResource()
+        val item= postContents[position]
+        Glide.with(context).load(item)
+            .override(500,1280)
+            .into(holder.postContent)
     }
 }
