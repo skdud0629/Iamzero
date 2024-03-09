1d9afdfdf35bd3111b826a08848988fd16519e92
nazero <skdud4081@gmail.com>
2024-03-09 14:51:07 +0900

    modify post data class field type

--- app/src/main/java/com/example/iamzero/data/diary/Post.kt
+++ app/src/main/java/com/example/iamzero/data/diary/Post.kt
@@ -1,13 +1,15 @@
 package com.example.iamzero.data.diary
 
+import android.net.Uri
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.util.ArrayList
 
 @Entity
 data class Post(
     @PrimaryKey
     val id: Long,
     val date: String,
-    val img: List<String>,
+    val img: ArrayList<String>?,
     val content: String
 )
