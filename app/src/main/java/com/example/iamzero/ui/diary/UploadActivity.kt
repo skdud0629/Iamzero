528f36e67e155b5891e218c9415878b5c81ed1da
nazero <skdud4081@gmail.com>
2024-03-09 15:53:58 +0900

    implement upload diary

--- app/src/main/java/com/example/iamzero/ui/diary/UploadActivity.kt
+++ app/src/main/java/com/example/iamzero/ui/diary/UploadActivity.kt
@@ -1,18 +1,37 @@
 package com.example.iamzero.ui.diary
 
-import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.iamzero.databinding.ActivityDiaryPostBinding
+import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
+import com.example.iamzero.DiaryViewModelFactory
+import com.example.iamzero.data.diary.DiaryRepositoryImpl
+import com.example.iamzero.data.diary.Post
+import com.example.iamzero.databinding.ActivityUploadBinding
+import java.time.LocalDate
 
 class UploadActivity : AppCompatActivity() {
-    private lateinit var _binding: ActivityDiaryPostBinding
+    private lateinit var _binding: ActivityUploadBinding
+    private val viewModel by viewModels<DiaryViewModel> { DiaryViewModelFactory(DiaryRepositoryImpl(this)) }
     val binding get() = _binding
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        _binding = ActivityDiaryPostBinding.inflate(layoutInflater)
+        _binding = ActivityUploadBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.postContentContainer.adapter=PostImagePagerAdapter(,Context)
+
+        val imageList = intent.getStringArrayListExtra("imageList")
+        val localDate: LocalDate = LocalDate.now()
+        val id: Long = System.currentTimeMillis()
+        binding.postContentContainer.adapter = PostImagePagerAdapter(imageList!!, this)
+
+        binding.uploadBtn.setOnClickListener {
+            val post = binding.postEt.text.toString()
+            viewModel.uploadDiary(Post(id, localDate.toString(), imageList, post))
+            finish()
+        }
     }
 
 
